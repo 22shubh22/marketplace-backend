01be1842a45ce9972b709a58bcb168a3d7edd469
Shubham Goyal <22shubh22@gmail.com>
2022-02-23 12:24:51 +0530

    swagger api docs generator

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'corsheaders',
 ]
 
